7f5eab5bd6e4e9645464273c30e82759f447b87d
Rafael Escribano <r.escribano1@gmail.com>
2021-09-29 22:32:41 -0400

    Update id of search text field

--- wp-content/themes/scribble_s/functions.php
+++ wp-content/themes/scribble_s/functions.php
@@ -146,7 +146,7 @@ add_action( 'widgets_init', 'scribble_s_widgets_init' );
 
 function add_search_form($items, $args) {
 	if( $args->theme_location == 'mobile_nav' )
-		$items .= '<li class="search"><form role="search" method="get" id="searchform" action="'.home_url( '/' ).'"><input type="text" value="search" name="s" id="s" /><input type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" /></form></li>';
+		$items .= '<li class="search"><form role="search" method="get" id="searchform" action="'.home_url( '/' ).'"><input type="text" value="search" name="s" id="searchfield" /><input type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" /></form></li>';
 	return $items;
 }
 add_filter('wp_nav_menu_items', 'add_search_form', 10, 2);
