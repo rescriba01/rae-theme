48a912fad513fdf9a6e1e8037c0c353a3f113a4e
Rafael Escribano <rafael.escribano@saltwaterco.com>
2021-10-04 12:41:26 -0400

    use jQuery hamburger from custom.js in optima

--- wp-content/themes/scribble_s/assets/js/modules/navigation-min.js
+++ wp-content/themes/scribble_s/assets/js/modules/navigation-min.js
@@ -1,103 +1,38 @@
-/**
- * File navigation.js.
- *
- * Handles toggling the navigation menu for small screens and enables TAB key
- * navigation support for dropdown menus.
- */
-
-const mobileNav = ( function navigation() {
-	const siteNavigation = document.getElementById( 'site-navigation' );
-	if ( ! siteNavigation ) {
-		return mobileNav;
-	}
-
-	const button = mobileSiteNavigation.getElementsByTagName( 'button' )[ 0 ];
-
-	// Return early if the button don't exist.
-	if ( 'undefined' === typeof button ) {
-		return;
-	}
-
-	const menu = mobileSiteNavigation.getElementsByTagName( 'ul' )[ 0 ];
-
-	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
-		button.style.display = 'none';
-		return;
-	}
-
-	if ( ! menu.classList.contains( 'nav-menu' ) ) {
-		menu.classList.add( 'nav-menu' );
-	}
-
-	// Toggle the .toggled class and the aria-expanded value each time the button is clicked.
-	button.addEventListener( 'click', function() {
-		mobileSiteNavigation.classList.toggle( 'toggled' );
-
-		if ( button.getAttribute( 'aria-expanded' ) === 'true' ) {
-			button.setAttribute( 'aria-expanded', 'false' );
-		} else {
-			button.setAttribute( 'aria-expanded', 'true' );
-		}
-	} );
-
-	// Check the viewport size add HTML5 global hidden attribute
-	// const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-
-	// Remove the .toggled class and set aria-expanded to false when the user clicks outside the navigation.
-	document.addEventListener( 'click', function( event ) {
-		const isClickInside = siteNavigation.contains( event.target );
-
-		if ( ! isClickInside ) {
-			siteNavigation.classList.remove( 'toggled' );
-			button.setAttribute( 'aria-expanded', 'false' );
-		}
-	} );
-
-	// Get all the link elements within the menu.
-	const links = menu.getElementsByTagName( 'a' );
-
-	// Get all the link elements with children within the menu.
-	const linksWithChildren = menu.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
-
-	// Toggle focus each time a menu link is focused or blurred.
-	for ( const link of links ) {
-		link.addEventListener( 'focus', toggleFocus, true );
-		link.addEventListener( 'blur', toggleFocus, true );
-	}
-
-	// Toggle focus each time a menu link with children receive a touch event.
-	for ( const link of linksWithChildren ) {
-		link.addEventListener( 'touchstart', toggleFocus, false );
-	}
-
-	/**
-	 * Sets or removes .focus class on an element.
-	 */
-	function toggleFocus() {
-		if ( event.type === 'focus' || event.type === 'blur' ) {
-			let self = this;
-			// Move up through the ancestors of the current link until we hit .nav-menu.
-			while ( ! self.classList.contains( 'nav-menu' ) ) {
-				// On li elements toggle the class .focus.
-				if ( 'li' === self.tagName.toLowerCase() ) {
-					self.classList.toggle( 'focus' );
-				}
-				self = self.parentNode;
+jQuery( document ).ready(
+	function( $ ) {
+		$( '.hideable' ).on(
+			'click',
+			function() {
+				$( this ).hide();
 			}
-		}
+		);
 
-		if ( event.type === 'touchstart' ) {
-			const menuItem = this.parentNode;
-			event.preventDefault();
-			for ( const link of menuItem.parentNode.children ) {
-				if ( menuItem !== link ) {
-					link.classList.remove( 'focus' );
-				}
+		// Navigation
+
+		/*$( 'header#masthead .search a, #search_frame a.search_close, #search_frame .bgclick' ).click(
+			function( e ) {
+				e.preventDefault();
+				$( '#search_frame' ).fadeToggle( 'fast' );
 			}
-			menuItem.classList.toggle( 'focus' );
-		}
+		);*/
+		$( '#mobile-nav ul li.menu-item-has-children>a' ).click(
+			function( e ) {
+				e.preventDefault();
+				$( this ).parent( 'li' ).children( 'ul.sub-menu' ).slideToggle( 'fast' );
+				$( this ).parent( 'li' ).toggleClass( 'expanded' );
+			}
+		);
+		$( 'ul.menu-navigation-contents li.menu-item-has-children>a' ).click(
+			function( e ) {
+				e.preventDefault();
+			}
+		);
+		$( 'header#masthead button.hamburger' ).click(
+			function() {
+				$( '#mobile-nav' ).toggleClass( 'active' );
+				$( this ).toggleClass( 'is-active' );
+				$( 'html,body' ).toggleClass( 'fixed' );
+			}
+		);
 	}
-}() );
-
-export default mobileNav;
+);
