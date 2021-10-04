ce873d765500e4938e332baca3816d8b72ab9ad8
Rafael Escribano <r.escribano1@gmail.com>
2021-10-04 07:16:02 -0400

    Remove container element around desktop nav

--- wp-content/themes/scribble_s/template-parts/header/navigation.php
+++ wp-content/themes/scribble_s/template-parts/header/navigation.php
@@ -1,50 +1,47 @@
-<nav id="" class="hhnh-desktop-nav" aria-label="Main" hidden>
-	<div id="site-navigation" class="split__container">
-		<?php wp_nav_menu(
-			array(
-				'theme_location'  => 'split_nav_left',                                              // where it's located in the theme.
-				'container'       => 'div',                                                         // add/remove nav container.
-				'container_class' => 'nav-container--left',                                         // class of container (should you choose to use it).
-				'menu'            => __( 'Split Nav Left', 'scribble_s' ),                          // nav name.
-				'menu_class'      => 'main-navigation-contents nav--split split--left',                     // adding custom nav class.
-				'before'          => '',                                                            // before the menu.
-				'after'           => '',                                                            // after the menu.
-				'link_before'     => '',                                                            // before each link.
-				'link_after'      => '',                                                            // after each link.
-				'depth'           => 0,                                                             // limit the depth of the nav.
-				'fallback_cb'     => '',                                                            // fallback function (if there is one).
-			)
-		); ?>
+<nav id="" class="hhnh-desktop-nav" aria-label="Main">
+	<?php wp_nav_menu(
+		array(
+			'theme_location'  => 'split_nav_left',                                              // where it's located in the theme.
+			'container'       => 'div',                                                         // add/remove nav container.
+			'container_class' => 'nav-container--left',                                         // class of container (should you choose to use it).
+			'menu'            => __( 'Split Nav Left', 'scribble_s' ),                          // nav name.
+			'menu_class'      => 'main-navigation-contents nav--split split--left',                     // adding custom nav class.
+			'before'          => '',                                                            // before the menu.
+			'after'           => '',                                                            // after the menu.
+			'link_before'     => '',                                                            // before each link.
+			'link_after'      => '',                                                            // after each link.
+			'depth'           => 0,                                                             // limit the depth of the nav.
+			'fallback_cb'     => '',                                                            // fallback function (if there is one).
+		)
+	); ?>
 
-		<div id="logo--centered">
-			<?php
-			$custom_logo_id = get_theme_mod( 'custom_logo' );
-			$logo           = wp_get_attachment_image_src( $custom_logo_id, 'full' );
-			if ( has_custom_logo() ) {
-				echo '<a href="/" class="logo"><img src="' . esc_url( $logo[0] ) . '" alt="HomeHelpNH"></a>';
-			}
-			?>
-		</div>
-
+	<div id="logo--centered">
 		<?php
-		wp_nav_menu(
-			array(
-				'theme_location'  => 'split_nav_right',                                             // where it's located in the theme.
-				'container'       => 'div',                                                         // add/remove nav container.
-				'container_class' => 'nav-container--right',                                        // class of container (should you choose to use it).
-				'menu'            => __( 'Split Nav Right', 'scribble_s' ),                         // nav name.
-				'menu_class'      => 'main-navigation-contents nav--split split--right',            // adding custom nav class.
-				'before'          => '',                                                            // before the menu.
-				'after'           => '',                                                            // after the menu.
-				'link_before'     => '',                                                            // before each link.
-				'link_after'      => '',                                                            // after each link.
-				'depth'           => 0,                                                             // limit the depth of the nav.
-				'fallback_cb'     => '',                                                            // fallback function (if there is one).
-			)
-		);
+		$custom_logo_id = get_theme_mod( 'custom_logo' );
+		$logo           = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+		if ( has_custom_logo() ) {
+			echo '<a href="/" class="logo"><img src="' . esc_url( $logo[0] ) . '" alt="HomeHelpNH"></a>';
+		}
 		?>
-
 	</div>
+
+	<?php
+	wp_nav_menu(
+		array(
+			'theme_location'  => 'split_nav_right',                                             // where it's located in the theme.
+			'container'       => 'div',                                                         // add/remove nav container.
+			'container_class' => 'nav-container--right',                                        // class of container (should you choose to use it).
+			'menu'            => __( 'Split Nav Right', 'scribble_s' ),                         // nav name.
+			'menu_class'      => 'main-navigation-contents nav--split split--right',            // adding custom nav class.
+			'before'          => '',                                                            // before the menu.
+			'after'           => '',                                                            // after the menu.
+			'link_before'     => '',                                                            // before each link.
+			'link_after'      => '',                                                            // after each link.
+			'depth'           => 0,                                                             // limit the depth of the nav.
+			'fallback_cb'     => '',                                                            // fallback function (if there is one).
+		)
+	);
+	?>
 </nav>
 
 <nav id="mobile-nav"  class="hhnh-mobile-nav">
